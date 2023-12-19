3cef9dc62853f1b8a5261c2325f00bf9b49ed249
Carojpp <carolyn.pena.padilla@gmail.com>
2023-12-19 08:40:10 -0500

    CM-003 modify routes-controllers add docs

--- api/src/controllers/dogsController.js
+++ api/src/controllers/dogsController.js
@@ -1,4 +1,32 @@
-/*const {Dogs} = require('../db')
-const axios = require('axios')
-const { URL_API } = process.env; */
+/*
+Conectaremos el api y a la base de datos
+*/
+const express = require('express');
+const axios = require('axios');
+const URL_API = 'https://thedogapi.com/'; // Esta es la ruta de la API que necesitamos para obtener las razas de los perros
 
+
+/* Esta función asincrónica getDogsDb consulta la base de datos para obtener todos los registros de Dogs, incluyendo los nombres de sus tipos asociados (Type) incluido a solo el atributo name.
+
+const getDogsDb = async ()=> {
+    const response = await Dogs.findAll({
+        include:{
+            attributes: ['name'],
+            model: Type,
+            through: {
+                attributes: [],
+                },
+        }
+    })
+        return response
+}
+*/
+
+//Como acceso un controlador desde la ruta? , debo importarlo en routes
+const getDogsController = () => {
+    return [] // retorna el arreglo vacio
+}
+
+module.exports = {
+    getDogsController
+};
--- api/src/routes/dogsRouter.js
+++ api/src/routes/dogsRouter.js
@@ -2,16 +2,33 @@
     Este Router va a tener las siguientes rutas 
     get -> '/dogs'
     post -> '/createDog'
+
+    Objetivo, cruzar por el enrutador, pasar a controllers y conectarlo con sequalize
 */ 
 const { Router } = require('express');
-
-const dogsRouter = Router();  //Instaciamos nuestro Router de express para poder agregar nuestras rutas
+const { getDogsController } = require('../controllers/dogsController.js'); // importamos la ruta desde controller
+const dogsRouter = Router();  //Instaciamos nuestro Router de express para poder agregar nuestras rutas 
 
 console.log('loding dogsRouter');
 
 dogsRouter.get('/dogs', (req, res) => {
   console.log('file: dogsRouter.js -> /dogs');
-  res.send('Dogs');
+  const result = getDogsController() // ejecutamos la funcion getDogsController(que son los datos que tenemos desde la API que relacinamos en controller)
+  res.send(result); // aca devuelve lo que le asignamos a result, el valor que da getDogsController
 });
 
-module.exports = dogsRouter;
+/*
+  El parametro que se ejecuta sera idRaza el cual me traera la raza especifica de un perro (que es cualquier raza que yo relacione, ejemplo golden, husky)
+    1. Obtener el idRaza de la variable req
+    2.
+  Para agregar otro tipo de parametro GET desde postman se usa la ruta que relacionamos en routes + ?otroparam=unvalor luego de la parametro que relacionamos en visual http://localhost:3001/dogs/husky?otroparam=unvalor
+*/
+dogsRouter.get('/dogs/:idRaza', (req, res) => {
+  console.log('file: dogsRouter.js -> /dogs/:idRaza')
+  const { params,query } = req
+  console.log({params,query})
+  //console.log(req)
+  res.send ({params, route:'/dogs/:idRaza'});
+});
+
+module.exports = dogsRouter; // Retornar todo lo que esta en dogsRouter
